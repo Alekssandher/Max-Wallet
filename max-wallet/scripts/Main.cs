4ee82814546ef65a282662f861fafad916e01d6e
Alekssandher <alekssandher1@hotmail.com>
2024-12-10 10:56:00 -0300

    feat: login and register using bcript

--- max-wallet/scripts/Main.cs
+++ max-wallet/scripts/Main.cs
@@ -2,55 +2,57 @@ using Godot;
 using System;
 using NBitcoin;
 using System.Security.Cryptography.X509Certificates;
+using BCrypt.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 public partial class Main : Node
 {	
 	private Button _createWalletButton;
-	
-	public string storedPassword = "";
+	public string inputPassword;
+	public string savePath = "saves/passInfo.json";
+	public int cost = 16;
 
 	[Export]
 	public LineEdit UserInput { get; set; }
-
+	[Export]
+	public Label Message {get; set;}
 	[Export]
 	public Button ActionButton { get; set;}
 	[Export]
 	public Label WarningLabel;
+	
 
 	public override void _Ready()
 	{	
 		WarningLabel.Visible = false;
 
-		if(storedPassword == "") {
+		if (File.Exists(savePath)) {
+			Message.Text = "Type your password";
+			ActionButton.Text = "Login";
+
+		}
+		else {
+			Message.Text = "Create your password";
 			ActionButton.Text = "Register password";
 
-		} 
-		else {
-			ActionButton.Text = "Login";
 		}
 		
 	}
 
+	public class PasswordInfo {
+		public string Salt { get; set; }
+		public string PasswordHash { get; set; }
+	}
 	private void _on_action_button_pressed() {
 
-		if (storedPassword == "") {
-			Cadastro();
-
-		}
-		else {
+		if (File.Exists(savePath)) {
 			Login();
 		}
+		else {
+			Cadastro();
+		}
 
-
-		// NBitcoin.Key privateKey = new NBitcoin.Key();
-
-		// PubKey publicKey = privateKey.PubKey;
-
-		// var publicKeyHash = publicKey.Hash;
-
-		// var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
-
-		// GD.Print("pressed: " + publicKey);
 	}
 
 	public void Aviso(string message, string color) {
@@ -60,21 +62,56 @@ public partial class Main : Node
 
 	}
 	
+	public object Encript(string inputPassword) {
+
+		var sw = System.Diagnostics.Stopwatch.StartNew();
+
+		string salt = BCrypt.Net.BCrypt.GenerateSalt(16);		
+		
+		string passwordHash =  BCrypt.Net.BCrypt.HashPassword(inputPassword + salt, workFactor: cost);
+		sw.Stop();
+
+		GD.Print("Levou " + sw.ElapsedMilliseconds + "ms");
+
+		var passInfo = new PasswordInfo {Salt = salt, PasswordHash = passwordHash};
+
+		return passInfo;
+	}
 	public void Cadastro() {
-		if(UserInput.Text == ""){ 
-				Aviso("Your password can't be empty", "bd0a33");
 
-			}
+		if(UserInput.Text == ""){ Aviso("Your password can't be empty", "bd0a33"); }
+		
+		inputPassword = UserInput.Text;
 
-			storedPassword = UserInput.Text;
+		object passInfo = Encript(inputPassword);
 
-			Aviso("Password successfuly registred", "5db900");
-			UserInput.Text = "";
+		string json = JsonConvert.SerializeObject(passInfo);
+
+		File.WriteAllText(savePath, json);
+		
+		Aviso("Password successfuly registred", "5db900");
+
+		UserInput.Text = "";
+
 	}
 
 	public void Login() {
-		if(UserInput.Text == storedPassword) {
-			GD.Print("loged");
+		string json = File.ReadAllText(savePath);
+
+		PackedScene posLog = ResourceLoader.Load<PackedScene>("res://scenes/PosLog.tscn");
+
+		PasswordInfo passInfo = JsonConvert.DeserializeObject<PasswordInfo>(json);
+
+		if (BCrypt.Net.BCrypt.Verify(UserInput.Text + passInfo.Salt, passInfo.PasswordHash)) {
+			Aviso("Loged successfuly", "5db900");
+
+			GetTree().ChangeSceneToPacked(posLog);
+
+			GD.Print("Logado");
+		}
+		else {
+			Aviso("Wrong password, try again", "bd0a33");
+			UserInput.Text = "";
 		}
 	}
 }
